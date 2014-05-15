43a7fd798a321a2c8be787a18e67b53484aec0eb
zhenchentl <zhenchentl@gmail.com>
2014-05-15 22:04:13 +0800

    add performance.py

---
+++ performance.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#coding=utf-8
+#########################################################################
+# File Name: performance.py
+# Author: Mark Chen
+# mail: zhenchentl@gmail.com
+# Created Time: 2014年05月15日 星期四 20时15分49秒
+#########################################################################
+
+import sys
+sys.path.append('..')
+
